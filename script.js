58b1922a7487262bf964c4ba6138c8359f0aaf4f
shashimehta03 <itsmeshashi29@gmail.com>
2024-10-12 13:50:48 +0530

    inti cmd

---
+++ script.js
@@ -0,0 +1,11 @@
+// Optional: You can add smooth scrolling for the navigation links
+document.querySelectorAll('.nav-menu a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
